5741a7f9648577d1396c0231ecd803df635815fb
MounikaMorla <morla.mounika000@gmail.com>
2025-01-09 10:00:24 +0530

    Your commit message

---
+++ project/finalproject.py
@@ -0,0 +1,36 @@
+import flask
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+@app.route('/')
+def check():
+    return "Welcome to SRGEC"
+
+with open('Final_Model.pkl','rb') as f:
+    model = pickle.load(f)
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    Rooms = int(request.form['bedrooms'])
+    Bathrooms = int(request.form['bathrooms'])
+    Place = int(request.form['location'])
+    Area = int(request.form['area'])
+    Status = int(request.form['status'])
+    Facing = int(request.form['facing'])
+    P_Type = int(request.form['type'])
+    #now take the above form data and convert to array
+    input_data = np.array([[Rooms,Area,Place,Status,Bathrooms,
+                            Facing,P_Type]])
+    #by taking above data we will predict the House_price
+    prediction = model.predict(input_data)[0]
+    #now we will pass above predicted data to template
+    return render_template('index.html',
+                           prediction = prediction)
+app.run()
+
+
